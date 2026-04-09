95ce47d61b1eec0e2165df46937d2e686f1cb712
Tihomir971 <tihomir.jev@outlook.com>
2026-04-09 16:33:48 +0200

    Add region_id to GetProductsByBrandSchema

--- src/api/middlewares.ts
+++ src/api/middlewares.ts
@@ -17,6 +17,7 @@ export const GetCategoryBrandsSchema = createFindParams().extend({
 export const GetProductsByBrandSchema = createFindParams().extend({
   category_id: z.union([z.string(), z.array(z.string())]).optional(),
   brand_id: z.union([z.string(), z.array(z.string())]).optional(),
+  region_id: z.string().optional(),
 })
 
 export default defineMiddlewares({
